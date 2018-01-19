b9d05097961e4e4f3333706a629993f5c00dfb9e
= <=>
2018-01-19 16:02:55 +0700

    Sửa menutop

---
+++ chitietsanpham.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="Style/Css.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+<title>Thông tin sản phẩm</title>
+</head>
+<body>
+	
+	<div >
+		<div class="header">
+			
+			<ul>
+			  <li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i> Sign Up</a></li>
+			  <li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a></li>
+			</ul>
+			<h1 style="text-align: center;"><strong>Thời Trang</strong></h1>
+			<img src="imags/ellen.jpg" style="height: 400px; width: 100%;"/>
+   			
+    	</div>
+	</div>
+        <!-- begin Menu top-->
+         <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top" id="menutop">
+          <a class="navbar-brand" href="#">Ellen</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+   			 <span class="navbar-toggler-icon"></span>
+         </button>
+          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          
+          <ul class="navbar-nav">
+            <li class="nav-item ">
+			  <a class="nav-link " href="#" >Thời Trang<i style="margin-left: 2px;" class="fa fa-angle-down" aria-hidden="true"></i></a>
+			  	<ul class=" sub-menu-top">
+			  		<li ><a  href="#" >Áo</a></li>
+			  		<li ><a  href="#" >Quần</a></li>
+			  		<li ><a  href="#" >Set</a></li>
+			  		<li ><a  href="#" >Thời trang dạo phố</a></li>
+			  		<li ><a  href="#" >Thời trang mùa hè</a></li>
+			  	</ul>
+			</li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Liên hệ</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+               <div >
+				 <form class="form-inline">
+					 <div class="input-group">
+					  
+					  <input type="text" class="form-control" placeholder="Search...">
+					  <button class="input-group-addon" type="submit">
+					  	<i class="fa fa-search" aria-hidden="true"></i>
+					  </button>
+					</div> 
+				  </form>
+				</div>
+            </li>
+           
+          </ul>
+			
+         
+         </div>
+          
+          
+        </nav>
+        <!-- end menutop-->
+        
+        
+        <div class="container-fluid"><br>
+			<div id="main">
+				<div class="row">
+					<div class="col-sm-2 col-md-2 col-lg-2"></div>
+					<div class="col-sm-8 col-md-8 col-lg-8" id="center"><!--Begin center-->
+						<div class="row">
+							<div class="col-sm-12 col-md-12 col-lg-12">
+								<h4>Váy đỏ quyến rủ</h4>
+							</div>
+							
+						</div>
+						<hr style="border: 1px solid #676666">
+						<div class="row">
+							<div class="col-sm-4 col-md-4 col-lg-4"><!--Begin Hinh mau-->
+								
+									 
+									<div class="card">
+									  <img src="imags/product2.jpg" alt="Jane" style="width:100%; height: 100%;">
+									  <div class="container">
+									  </div>
+									  
+									</div>
+									<div id="video"><!--Begin video-->
+											<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal" style="width: 100%;">
+											<i class="fa fa-youtube-play" aria-hidden="true"></i>
+										View more
+									  </button>
+
+									  <!-- The Modal -->
+									  <div class="modal fade" id="myModal">
+										<div class="modal-dialog modal-lg">
+										  <div class="modal-content">
+
+											<!-- Modal Header -->
+											<div class="modal-header">
+											  <h4 class="modal-title">Video mẫu</h4>
+											  <button type="button" class="close" data-dismiss="modal">&times;</button>
+											</div>
+
+											<!-- Modal body -->
+											<div class="modal-body">
+											  <iframe width="100%" height="450" src="https://www.youtube.com/embed/psuRGfAaju4" allowfullscreen>
+												</iframe>
+											</div>
+
+											<!-- Modal footer -->
+											<div class="modal-footer">
+											  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+											</div>
+
+										  </div>
+										</div>
+									  </div>
+									</div><!--End Video-->
+								
+								
+							</div><!--End hình mau-->
+							<div class="col-sm-6 col-md-6 col-lg-6">
+								<div class="row">
+									<div class="col-sm-12 col-md-12 col-lg-12">
+										<h4>Váy đỏ quyến rủ</h4>
+										<span class="sale">250.000đ</span>
+										<span class="price">250.000đđ</span><br>
+										
+										<p>
+											Phong cách thời trang nam giới nói riêng luôn có sự thay đổi theo thời gian. Tuy nhiên, những trang phục thuộc diện thì vẫn có được chỗ đứng cho mình trong lòng công chúng. Món đồ mà chúng tôi đang muốn nói đến ở đây là áo sơmi trắng....
+										</p>
+										<hr>
+										<br>
+									
+										<form><!--So luong san pham-->
+											<label>Sô lượng:</label>
+											<input type="number" value="1" max="69" min="0" id="number" name="number"
+											 style="text-align: center;width: 100px;height: 45px;"></input>	
+											 <button class="btn btn-warning" 
+   											style="height: 45px;margin-top: -6px;" type="submit">
+   											<i class="fa fa-cart-arrow-down" aria-hidden="true"
+   												style="font-size: 22px;margin-right: 6px;"
+   											></i>Thêm vào giỏ hàng</button>
+										</form>
+									</div>
+
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="tab">
+							  <button class="tablinks" onclick="openCity(event, 'London')">Thông tin sản phẩm</button>
+							  <button class="tablinks" onclick="openCity(event, 'Paris')">Hướng dẫn mua hàng</button>
+							  <button class="tablinks" onclick="openCity(event, 'Tokyo')">Đánh giá chi tiết</button>
+							</div>
+
+							<div id="London" class="tabcontent">
+							  
+							  <p>Thông tin sản phẩm</p>
+							</div>
+
+							<div id="Paris" class="tabcontent">
+							  
+							  <p>Hướng dẫn mua hàng</p> 
+							</div>
+
+							<div id="Tokyo" class="tabcontent">
+							 
+							  <p>Đánh giá chi tiết</p>
+							</div>
+							<br>
+							<div class="row sanpham" ><!--Begin san pham lien quan-->
+							<div class="col-sm-12 col-md-12 col-lg-12">
+								<div  id="heading">
+									<h2 id="title-head">
+										<a href="#">Sản phẩm liên quan</a>
+									</h2>
+								</div>
+								
+								<div class="row">
+							  
+								  <div class="column">
+									<div class="card">
+									  <img src="imags/product1.jpg" alt="Jane" style="width:100%">
+									  <div class="container">
+										<h5 style="text-align: center;"><a href="#" 
+										style="color: #252525;text-decoration: none;">Jane Doe</a></h5>
+											<h6 id="sale">250.000đ</h6>
+											<h6 id="price"></h6>
+											
+										<p><button class="button" type="submit">Mua</button></p>
+									  </div>
+									</div>
+								  </div>
+
+								  <div class="column">
+									<div class="card">
+									  <img src="imags/product5.jpg" alt="Mike" style="width:100%">
+									  <div class="container">
+										<h5 style="text-align: center;"><a href="#" 
+										style="color: #252525;text-decoration: none;">Jane Doe</a></h5>
+											<h6 id="sale">250.000đ</h6>
+											<h6 id="price">250.000đ</h6>
+										<p><button class="button" type="submit">Mua</button></p>
+									  </div>
+									</div>
+								  </div>
+
+								  <div class="column">
+									<div class="card">
+									  <img src="imags/product2.jpg" alt="John" style="width:100%">
+									  <div class="container">
+										<h5 style="text-align: center;"><a href="#" 
+										style="color: #252525;text-decoration: none;">Jane Doe</a></h5>
+											<h6 id="sale">250.000đ</h6>
+											<h6 id="price">250.000đ</h6>
+										<p><button class="button" type="submit">Mua</button></p>
+									  </div>
+									</div>
+								  </div>
+								</div>
+								 <div class="row">
+								  	<div class="col-sm-12 col-md-12 col-lg-12">
+								  		<div class="pagination">
+										  <a href="#">Xem thêm>></a>
+										  
+									</div>
+								  	</div>
+								  </div>	
+								
+							</div>
+						</div><!--End san liên quan-->
+					</div><!--End center-->
+					
+				</div>
+			</div>
+			
+        </div>
+        <hr>
+        <br>
+    	<footer class="clearfix" id="footer">
+    		<div class="container">
+    			<div class="row">
+    				<div class="col-sm-4 col-md-4 col-lg-4 widget-item" id="about">
+    					<a href="#">Logo</a>
+    					<ul>
+    						<li>
+    							<i class="fa fa-map-marker" aria-hidden="true" style="font-size: 20px;text-indent: 4px;"></i>
+    							135/13A,Tăng Nhơn Phú B,Tp.Hồ Chí Minh
+    						</li>
+    						<li>
+    							<i class="fa fa-phone" aria-hidden="true" style="font-size: 20px; text-indent: 3px;"></i>
+    							01659548682
+    						</li>
+    						<li>
+    							<i class="fa fa-envelope" aria-hidden="true" style="font-size: 20px;"></i>
+    							tvtri1997@gmail.com
+    						</li>
+    					</ul>
+    				</div>
+    				<div class="col-sm-2 col-md-2 col-lg-2 widget-item">
+    					<h4 class="widget-title">Tài khoản</h4>
+    					<ul class="widget-menu">
+    						<li>
+    							<a href="#">Trang chủ</a>
+    						</li>
+    						<li>
+    							<a href="#">Sản phẩm</a>
+    						</li>
+    						<li>
+    							<a href="#">Tin tức</a>
+    						</li>
+    						<li>
+    							<a href="#">Liên hệ</a>
+    						</li>
+    					</ul>
+    					
+    				</div>
+    				
+    				<div class="col-sm-2 col-md-2 col-lg-2 widget-item">
+    					<h4 class="widget-title">Chính sách</h4>
+    					<ul class="widget-menu">
+    						<li>
+    							<a href="#">Chính sách mua hàng</a>
+    						</li>
+    						<li>
+    							<a href="#">Chính sách đãi ngộ</a>
+    						</li>
+    						<li>
+    							<a href="#">Đăng kí thành viên</a>
+    						</li>
+    						<li>
+    							<a href="#">Chính sách đổi trả</a>
+    						</li>
+    					</ul>
+    					
+    				</div>
+    				
+    				 <div class="col-sm-2 col-md-2 col-lg-2 widget-item">
+    					<h4 class="widget-title">Điều khoản</h4>
+    					<ul class="widget-menu">
+    						<li>
+    							<a href="#">Đăng kí tài khoản</a>
+    						</li>
+    						<li>
+    							<a href="#">Chính sách bảo hành</a>
+    						</li>
+    						<li>
+    							<a href="#">Chính sách đổi trả</a>
+    						</li>
+    						<li>
+    							<a href="#">Thông tin cá nhân</a>
+    						</li>
+    					</ul>
+    					
+    				</div>
+    				<div class="col-sm-2 col-md-2 col-lg-2 widget-item">
+    					<h4 class="widget-title">Hướng dẫn</h4>
+    					<ul class="widget-menu">
+    						<li>
+    							<a href="#">Hướng dẫn mua hàng</a>
+    						</li>
+    						<li>
+    							<a href="#">Hướng dẫn thanh toán</a>
+    						</li>
+    						<li>
+    							<a href="#">Điều khoản dịch vụ</a>
+    						</li>
+    						<li>
+    							<a href="#">Thông tin cá nhân</a>
+    						</li>
+    					</ul>
+    					
+    				</div>
+    			</div>
+    		</div>
+    		<div class="bottom-footer">
+    			<div class="container">
+    				<div class="row">
+    					<div class="col-sm-6 col-md-6 col-lg-6 text-left">
+    						<span class="info-website">
+    							© Bản quyền thuộc về TT Team
+    						</span>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    	</footer>
+
+
+ <script>/* Bengin Tab chi tiet san pham*/
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+</script><!-- end Tab chi tiet san pham-->
+ 
+</body>
+</html>
